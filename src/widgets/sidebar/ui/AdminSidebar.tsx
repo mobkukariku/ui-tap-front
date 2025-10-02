dc76f9aec3749bc704a3175d07f9625d2d84d0d3
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-10-02 17:52:51 +0500

    feat: added /admin page

--- src/widgets/sidebar/ui/AdminSidebar.tsx
+++ src/widgets/sidebar/ui/AdminSidebar.tsx
@@ -7,7 +7,13 @@ import {
     SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem
 } from "@/shared/ui/sidebar";
 import {Avatar, AvatarFallback, AvatarImage} from "@/shared/ui/avatar";
-import {LayoutDashboardIcon, Settings, User} from "lucide-react";
+import {EllipsisVertical, LayoutDashboardIcon, LogOut, Settings, User} from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/shared/ui/dropdown-menu";
+import {Button} from "@/shared/ui/button";
 
 const items = [
     {
@@ -33,11 +39,12 @@ const items = [
 ]
 
 export function AdminSidebar() {
+
     return (
         <Sidebar>
             <SidebarHeader className={"p-4"}>
                 <figure
-                    className={"flex items-center gap-2"}>
+                    className={"flex items-center gap-2 justify-between"}>
                     <Avatar className={"w-10 h-10"}>
                         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
                         <AvatarFallback>CN</AvatarFallback>
@@ -51,6 +58,21 @@ export function AdminSidebar() {
                             Admin
                         </p>
                     </figcaption>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant={"ghost"} size={"sm"}>
+                                <EllipsisVertical />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>
+                                    <LogOut />
+                                    Выйти
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </figure>
             </SidebarHeader>
             <SidebarContent className={"p-4"}>
