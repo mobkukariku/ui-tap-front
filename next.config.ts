b3eec693511f8ad271e0217960d3ed84089cdfc8
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-12-01 23:57:39 +0500

    fixed workdlows dir

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const BUCKET_URL = process.env.NEXT_PUBLIC_BUCKET_URL;
 
 if (!API_URL) {
     throw new Error("API_URL environment variable is not set");
 }
 
-if (!BUCKET_URL) {
-    throw new Error("BUCKET_URL environment variable is not set");
-}
+
 
 const nextConfig: NextConfig = {
     output: 'standalone',
@@ -18,10 +15,6 @@ const nextConfig: NextConfig = {
             {
                 source: "/api/:path*",
                 destination: `${API_URL}/api/:path*`
-            },
-            {
-                source: "/accommodation-images/:paths*",
-                destination: `${BUCKET_URL}/accommodation-images/:paths*`
             }
         ];
     },
