68fa44e2886c3ba849033923de41a98b2f602d20
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-12-02 17:38:30 +0500

    fixed deploy.yml

--- src/shared/lib/date/formateDate.ts
+++ src/shared/lib/date/formateDate.ts
@@ -1,4 +1,5 @@
-export const formatDate = (dateStr: string) => {
+export const formatDate = (dateStr: string | undefined) => {
+    if(!dateStr) return '';
     const [year, month, day] = dateStr.split('-')
     return `${day}.${month}.${year}`
 }
