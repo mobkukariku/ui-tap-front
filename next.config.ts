4c37d2222a943d5b3e4341c26396a71af8bda381
Damir Tagankhozhaev <155202834+mobkukariku@users.noreply.github.com>
2025-12-02 15:33:47 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
 
-if (!API_URL) {
-    throw new Error("API_URL environment variable is not set");
-}
 
 
 
